94ee85a63d8adb17da88168e10ec715171dc35a3
Alan Mendoza <alanmendoza9801@outlook.com>
2023-01-03 22:52:30 +0000

    Se solucionaron warnings

--- Exercises/convertir_cadena_caracteres.py
+++ Exercises/convertir_cadena_caracteres.py
@@ -1,12 +1,15 @@
+"This file is a replacing pattern example with Python"
+
 # Import the regular expression module
 import re
 
 # Initialize the string to be modified
-t = "La 1 - en + la () de su LYA1 con el LYA1 final 3 5 puntos MO"
+T = "La 1 - en + la () de su LYA1 con el LYA1 final 3 5 puntos MO"
 
 # Replacing the pattern:
 # The r before a string indicates that the string is a raw string. A raw string is a string literal
-# that is not interpreted by the Python interpreter, so all characters in the string are taken literally.
+# that is not interpreted by the Python interpreter, so all characters in the string
+#  are taken literally.
 # We use \b only if it is a complete word (surrounded by word boundaries). Characters are
 # word boundary assertions.
 # "\s*\" is a regular expression pattern that matches zero or more white space characters.
@@ -17,10 +20,11 @@ t = "La 1 - en + la () de su LYA1 con el LYA1 final 3 5 puntos MO"
 # pattern: This is the regular expression pattern to search for.
 # replacement: This is the string to use as a replacement for the pattern.
 # string: This is the string to search and replace in.
-# re.sub() returns a new string with all occurrences of the pattern replaced by the replacement string.
+# re.sub() returns a new string with all occurrences of the pattern replaced
+#  by the replacement string.
 
 # For example, consider the following code:
-nc = re.sub(r"\b1\b", "primera", t)
+nc = re.sub(r"\b1\b", "primera", T)
 nc = re.sub(r"-", "persona", nc)
 nc = re.sub(r"\+", "mandarme", nc)
 nc = re.sub(r"\(\s*\)", "foto", nc)
