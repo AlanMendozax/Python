cdc83d2dae0ae80467b4aa3f3d830aec05cf2252
Alan Mendoza <alanmendoza9801@outlook.com>
2023-01-03 14:17:46 -0600

    Update ConvertirCadenaCaracteres.py
    I have added word boundary \b assertions to the patterns "3", "MO" and "1" to ensure that they only match when they are complete words, rather than when they appear as part of another word. I have also removed the unnecessary re.compile() calls and replaced them with calls to re.sub().

--- Exercises/ConvertirCadenaCaracteres.py
+++ Exercises/ConvertirCadenaCaracteres.py
@@ -1,10 +1,10 @@
 import re
-t = "La 1 - en  + la () de su LYA1 con el LYA1 final 3 5 MO"
-nc= re.compile("1").sub("primera",t)
-nc= re.compile("-").sub("persona",nc)
-nc= re.compile("\+").sub("mandarme",nc)
-nc= re.compile("()").sub("foto",nc)
-nc= re.compile("LYA1").sub("programa",nc)
-nc= re.compile("3").sub("obtiene",nc)
-nc= re.compile("MO").sub("extra",nc)
+t = "La 1 - en  + la () de su LYA1 con el LYA1 final 3 5 puntos MO"
+nc = re.sub(r"\b1\b", "primera", t)
+nc = re.sub(r"-", "persona", nc)
+nc = re.sub(r"\+", "mandarme", nc)
+nc = re.sub(r"\(\s*\)", "foto", nc)
+nc = re.sub(r"LYA1", "programa", nc)
+nc = re.sub(r"\b3\b", "obtiene", nc)
+nc = re.sub(r"\bMO\b", "extra", nc)
 print(nc)
